3cbcfbfb1892278b16717b78220c07b1d93f9ba7
Seung-hwan285 <ghks285@naver.com>
2022-09-21 18:20:44 +0900

    feat : 다른 URL로 접속하면 404

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,27 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import logo from './logo.svg';
 import './css/Navbar.scss';
 import "./App.css";
 import {BrowserRouter} from "react-router-dom";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link, Navigate } from "react-router-dom";
 import Home from "./Home/Home";
 import Cart from "./Cart/Cart";
 import Navbar from "./Home/Navbar";
+import NotFound from "./Home/NotFound";
+
+
 function App() {
-  return (
+    return (
     <div className="App">
+
 
       <BrowserRouter>
           <Navbar/>
         <Routes>
-
-          <Route path="/" element={<Home/>}/>
-          <Route path="/Cart" element={<Cart/>}/>
-
+            <Route path="/" element={<Home/>}/>
+            <Route path="/Cart" element={<Cart/>}/>
+            <Route path="/not-found" element={<NotFound/>}/>
+            <Route path="*" element={<Navigate to="/not-found"/>}/>
         </Routes>
 
       </BrowserRouter>
---
+++ src/Home/NotFound.tsx
@@ -0,0 +1,12 @@
+
+
+function NotFound() :JSX.Element{
+
+
+    return (
+        <div className="not-fount-container">
+            <h2>404 Error</h2>
+        </div>
+    )
+}
+export default NotFound;
